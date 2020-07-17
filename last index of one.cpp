ddc4dfa3dc42f851d376108aedf91b3fd4a3b802
Nehal Tiwari <nehaltiwari1599@gmail.com>
2020-07-17 16:31:28 +0530

    Create last index of one.cpp

---
+++ last index of one.cpp
@@ -0,0 +1,20 @@
+/*OM NAMAH SHIVAY!*/
+using namespace std;
+int main()
+ {
+     int t;
+     cin >> t;
+     for(int q=0;q<t;q++)
+     {
+         int ind=-1;
+         string s;
+         cin >> s;
+         for(int i=0;i<s.length();i++)
+         {
+             if(s.at(i)=='1')
+                ind=i;
+         }
+         cout << ind << endl;
+     }
+	return 0;
+}
