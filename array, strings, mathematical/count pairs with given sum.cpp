d6f23759fb9e731fbfd8bbb00918bcbb5ac87755
Nehal Tiwari <nehaltiwari1599@gmail.com>
2020-08-22 15:25:05 +0530

    Create count pairs with given sum.cpp

---
+++ array, strings, mathematical/count pairs with given sum.cpp
@@ -0,0 +1,25 @@
+/*OM NAMAH SHIVAY!*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k,count=0;
+	    cin>>n>>k;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=i+1;j<n;j++){
+	            if(a[i]+a[j]==k){
+	                count++;
+	            }
+	        }
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
