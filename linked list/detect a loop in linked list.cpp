7f9533dc9aab8374573bbb01359c49c9bdfa2eb4
Nehal Tiwari <nehaltiwari1599@gmail.com>
2020-10-03 14:52:41 +0530

    Create detect a loop in linked list.cpp
    https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1/?category[]=Linked%20List&page=1&sortBy=submissions&query=category[]Linked%20Listpage1sortBysubmissions

---
+++ linked list/detect a loop in linked list.cpp
@@ -0,0 +1,25 @@
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+
+bool detectLoop(Node* head)
+{
+    /*OM NAMAH SHIVAY*/
+    Node *slow=head;
+    Node *fast=head;
+    while(slow && fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
